a74c28db526b88999d72f1823143953ea4f8f6e8
sollabeck <kevin.ceballos@gmail.com>
2015-08-13 14:30:22 -0700

    remove routes generated by rails generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,10 @@
 Rails.application.routes.draw do
   
-  get 'tweets/index'
-
-  get 'search/index'
-
-  get 'keywords/index'
-
-  get 'home/index'
-
   #Homepage
   root 'home#index'
 
   #Search
   get '/search' => 'search#index'
-
-  #Keywords
-  get '/keywords' => 'keywords#index'
 
   #Tweets
   get '/keywords/:keyword_id/tweets'  => 'tweets#index', as: 'tweets'
